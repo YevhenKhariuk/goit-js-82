62be4d9dff26fcb985acee2b68f703cec52b24e5
YevhenKhariuk <777genya2009@gmail.com>
2023-04-13 23:17:38 +0300

    begin

---
+++ test-1.js
@@ -0,0 +1,18 @@
+// const apple = 47;
+// const grapes = 135;
+// const total = apple + grapes;
+// const diff = grapes - apple;
+// console.log(total);
+// console.log(diff);
+
+// let students = 100;
+// students += 50;
+// console.log(students);
+
+// console.log(Math.random() * (10 - 1) + 1);
+
+// const value = 27.9;
+// console.log("original", value);
+// console.log("floor", Math.floor(value));
+// console.log("ceil", Math.ceil(value));
+// console.log("round", Math.round(value));
