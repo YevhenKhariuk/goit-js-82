2d65bad194ac6f8fa810cdb46a785385b4f823b0
YevhenKhariuk <777genya2009@gmail.com>
2023-04-25 17:15:34 +0300

    13451463

---
+++ blended-3.js
@@ -0,0 +1,22 @@
+// 1) Напишіть функці copyArr(arr), яка копіює масив не змінюючи оригінал.
+
+// const vegetables = ["капуста", "ріпа", "редиска", "морква"];
+// function arrayClone(arr) {
+//     return arr.slice();
+
+// }
+// const array = arrayClone(vegetables);
+
+// const vegetables = ["капуста", "ріпа", "редиска", "морква"];
+// function arrayClone(arr) {
+//   return [...arr];
+// }
+// const array = arrayClone(vegetables);
+// console.log(array);
+
+const vegetables = ["капуста", "ріпа", "редиска", "морква"];
+function arrayClone(arr) {
+  return [].concat(arr);
+}
+const array = arrayClone(vegetables);
+console.log(array);
